11b20001815c3b1ee5dbd2a372c862a3fba6606c
eileencodes <eileencodes@gmail.com>
2019-08-21 10:55:29 -0400

    Install old tmate with new libevent
    The newest version of tmate isn't working for me and the old one wants
    an older libevent. This change puts back the old sha for the older
    version of tmate but linked to the new libevent.

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -1,15 +1,13 @@
 class Tmate < Formula
   desc "Instant terminal sharing"
   homepage "https://tmate.io/"
-  url "https://github.com/tmate-io/tmate/archive/2.3.0.tar.gz"
-  sha256 "f837514edbc19180d06b27713628466e68aba91654d748d6e65bfad308f5b00a"
+  url "https://github.com/tmate-io/tmate/archive/2.2.1.tar.gz"
+  sha256 "d9c2ac59f42e65aac5f500f0548ea8056fd79c9c5285e5af324d833e2a84c305"
   head "https://github.com/tmate-io/tmate.git"
 
   bottle do
     cellar :any
-    sha256 "92f51ef9496b1877391c008ae7f3a6a5a072ad2c0e5e52b2cebc85d9b98d9235" => :mojave
-    sha256 "fe9d988b1e3dbf7f3b2e1beecfdbc5ff25d2331118399331b1477f5b0f0efbb4" => :high_sierra
-    sha256 "75df441f4152ecb8f0e04276d7ecc1b2d348503efab8cda5296c2f02c6f38248" => :sierra
+    sha256 "a3a692cff9eba1a68eef998577050169da2eb3930e9e12902a33c689ed904548" => :sierra
   end
 
   depends_on "autoconf" => :build
